f213fb4d85d5753d8df3f2eedcc45d711c0b7dde
Christopher Simeus <e1639966@cmaisonneuve.qc.ca>
2024-03-07 13:28:50 -0500

    S5C2 - Nav bar en cours

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,11 +17,13 @@
 <body>
 
     <div id="menu__entete" class="global">
-        <input type="checkbox" name="" id="chk_burger">
-        <label id="burger" for="chk_burger">
-            <svg  width="48" height="48" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#fff"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </label>
         <header class="menu__header">
+            <input type="checkbox" name="" id="chk_burger">
+            <label id="burger" for="chk_burger">
+                <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#fff">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </label>
             <?php wp_nav_menu(array("container" => "nav")) ?>
         </header>
     </div>
